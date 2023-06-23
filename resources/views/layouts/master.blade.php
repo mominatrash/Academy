1b83befd25aeef38b55fb2b601ed1fb742c5399b
mominatrash <moamennew@gmail.com>
2023-06-23 22:27:34 +0300

    Vuexy Setup

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html class="loading" lang="en" data-textdirection="rtl">
+
+@include('layouts.head')
+
+@yield('css')
+
+<!-- BEGIN: Body-->
+
+<body class="vertical-layout vertical-menu-modern  navbar-floating footer-static  " data-open="click"
+    data-menu="vertical-menu-modern" data-col="">
+
+    <!-- BEGIN: Header-->
+    @include('layouts.navbar')
+    <!-- END: Header-->
+
+
+    <!-- BEGIN: Main Menu-->
+    @include('layouts.sidebar')
+    <!-- END: Main Menu-->
+
+    <!-- BEGIN: Content-->
+    <div class="app-content content ">
+        <div class="content-overlay"></div>
+        <div class="header-navbar-shadow"></div>
+        <div class="content-wrapper container-xxl p-0">
+            <div class="content-header row">
+            </div>
+            <div class="content-body">
+
+                @yield('content')
+
+            </div>
+        </div>
+    </div>
+
+
+    <!-- END: Content-->
+
+
+
+    <!-- BEGIN: Footer-->
+
+    @include('layouts.footer')
+
+    @yield('scripts')
+
+</body>
+<!-- END: Body-->
+
+</html>
